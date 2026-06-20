9841143b9f55e5a9e86b84276077d158932f4ce7
mr-mohamed-rashed <mrrashed0777@gmail.com>
2026-06-20 07:59:08 +0300

    Fix: Cap chat height to perfectly match video height on desktop to prevent expansion

--- src/pages/LiveMatches.tsx
+++ src/pages/LiveMatches.tsx
@@ -68,8 +68,10 @@ const LiveMatches = () => {
             <div className="relative shrink-0 w-full lg:max-w-3xl lg:flex-1 md:h-auto z-40">
               <Live2DTracker match={featured || { id: 'main', home: { name: 'A', color: '#888' }, away: { name: 'B', color: '#aaa' } } as any} hideSocials={true} />
             </div>
-            <div className="flex-1 min-h-0 w-full lg:w-[400px] xl:w-[450px] shrink-0 h-full lg:h-auto">
-              <LiveChat matchId={featured?.id?.toString() || 'main_live_stream'} isTheaterSplit={true} />
+            <div className="flex-1 min-h-0 w-full lg:w-[400px] xl:w-[450px] shrink-0 h-full lg:h-auto relative flex flex-col">
+              <div className="flex-1 lg:absolute lg:inset-0">
+                <LiveChat matchId={featured?.id?.toString() || 'main_live_stream'} isTheaterSplit={true} />
+              </div>
             </div>
           </section>
         ) : (
